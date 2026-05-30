4cea72824264c83e4158a9b7f04aa13988dfce71
tasuin40014 <86833528+tasuin40014@users.noreply.github.com>
2026-05-30 13:49:48 +0800

    Update __init__.py

--- components/fujitsu_ac/__init__.py
+++ components/fujitsu_ac/__init__.py
@@ -1,1 +1,12 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
+# 定義這個組件的代號
+CODEOWNERS = ["@your_github_handle"]
+DEPENDENCIES = ["uart"]
+
+fujitsu_ac_ns = cg.esphome_ns.namespace("fujitsu_ac")
+
+CONFIG_SCHEMA = cv.Schema({}).extend(cv.COMPONENT_SCHEMA)
